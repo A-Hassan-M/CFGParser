d30884543e77caf991a85df8c4011aee35d442d0
Sameh16 <sameh_mohamed16@hotmail.com>
2018-04-25 15:56:43 +0200

    test_Way

--- src/Parser/NonTerminals/ArrayBrackets.java
+++ src/Parser/NonTerminals/ArrayBrackets.java
@@ -4,13 +4,23 @@ import java.util.ArrayList;
 
 //TODO::Implement this class
 public class ArrayBrackets extends Node{
-    @Override
-    public String getValue() {
-        return null;
-    }
-
+    
+	public ArrayBrackets() {
+		classTokens = new ArrayList<>();
+		classTokens.add(new TerminalNode("< LEFT_SQUARE_B >", "["));
+		classTokens.add(new TerminalNode("RIGHT_SQUARE_B", "]"));
+	}
+	
     @Override
     public int matches(ArrayList<Node> tokens) {
-        return 0;
+        int i= 0;
+        while(!(tokens.isEmpty()) && i<classTokens.size()){
+            int matched = classTokens.get(i).matches(tokens);
+            if(matched==0){
+                return -1;
+            }
+            i++;
+        }
+        return 1;
     }
 }
--- src/Parser/NonTerminals/Extention.java
+++ src/Parser/NonTerminals/Extention.java
@@ -4,14 +4,24 @@ import java.util.ArrayList;
 
 //TODO::Implement this class
 public class Extention extends Node{
-    @Override
-    public String getValue() {
-        return null;
-    }
 
-    @Override
+	public Extention() {
+		classTokens  =  new ArrayList<>();
+		classTokens.add(new TerminalNode("< EXTENDS >", "extends"));
+		classTokens.add(new TerminalNode("< ID >","x"));
+	}
+	
+   @Override
     public int matches(ArrayList<Node> tokens) {
-        return 0;
+        int i= 0;
+        while(!(tokens.isEmpty()) && i<classTokens.size()){
+            int matched = classTokens.get(i).matches(tokens);
+            if(matched==0){
+                return -1;
+            }
+            i++;
+        }
+        return 1;
     }
 
 }
